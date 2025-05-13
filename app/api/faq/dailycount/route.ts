d43fe24be0c48cc93b55e6f8792c69a28aeb0f81
ppabam <tom@diginori.com>
2025-05-13 10:53:48 +0900

    fix api

---
+++ app/api/faq/dailycount/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from 'next/server';
+import { openDb } from '@/lib/db';
+
+export async function GET() {
+  try {
+    const db = await openDb();
+    
+    const sql = `
+      SELECT 
+        f.dutySctnNm,
+        COUNT(*) AS cnt 
+      FROM
+        faq_de d 
+        JOIN faq f ON d.faqNo = f.faqNo
+      GROUP BY dutySctnNm
+    `;
+    const result = await db.all(sql);
+    const total_tqapttn = result.find(r => r.dutySctnNm === 'tqapttn')?.cnt || 0;
+    const total_tqaplcy = result.find(r => r.dutySctnNm === 'tqaplcy')?.cnt || 0;
+
+    const sql_group_by = `
+    SELECT
+      SUBSTR(regDate, 1, 4) || '-' || SUBSTR(regDate, 5, 2) || '-' || SUBSTR(regDate, 7, 2) AS regDate,
+      dutySctnNm,
+      cnt
+    FROM (
+      SELECT 
+      d.regDate, 
+      f.dutySctnNm,
+      COUNT(*) cnt 
+      FROM
+        faq_de d JOIN faq f
+      ON 
+        d.faqNo = f.faqNo
+      GROUP BY d.regDate, f.dutySctnNm
+      ORDER BY d.regDate
+    )`;
+    const result_group_by = await db.all(sql_group_by);
+    
+    return NextResponse.json(
+      { 
+        민원: total_tqapttn,
+        정책: total_tqaplcy,
+        group_by: result_group_by
+      }
+    )
+  } catch (error) {
+    const err_msg = 'daily count 조회 오류'; 
+    console.error(err_msg, error);
+    return NextResponse.json({ error: err_msg }, { status: 500 });
+  }
+}
